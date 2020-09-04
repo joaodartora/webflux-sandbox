b52569d3f5233d58859fc07c8a201cb749e44fa9
João Dartora <joao.dartora@ilegra.com>
2020-09-04 00:00:00 -0300

    feat: creating schedulers project structure

---
+++ webflux-schedulers/src/main/java/com/joaodartora/webfluxschedulers/WebfluxschedulersApplication.java
@@ -0,0 +1,13 @@
+package com.joaodartora.webfluxschedulers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxschedulersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxschedulersApplication.class, args);
+	}
+
+}
---
+++ webflux-schedulers/src/test/java/com/joaodartora/webfluxschedulers/WebfluxschedulersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodartora.webfluxschedulers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxschedulersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
